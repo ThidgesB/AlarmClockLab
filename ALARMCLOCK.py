4e979bffc64ac691eaee93c72e5c421cd2c54022
ckatjrdavis <ckatjrdavis@gmail.com>
2021-12-23 13:29:45 -0600

    Built the AlarmClock Class and insantiated it in main.py

---
+++ ALARMCLOCK.py
@@ -0,0 +1,41 @@
+class alarm_clock:
+    def __init__(self):
+        self.current_time = '0000'
+        self.alarm_on = False
+        self.alarm_off = True
+        self.alarm_time = '0000'
+
+    def set_time(self):
+        self.current_time = input(f'Current time is {self.current_time}. Please set your desired time.')
+        if self.current_time != '0000':
+            print(f'{self.current_time} is the set time.')
+        
+    def set_alarm_time(self):
+        self.alarm_time = input(f'Alarm is set to {self.alarm_time}. Please enter your desired alarm time').lower().strip()
+        if self.alarm_time != '0000':
+            print(f'The alarm is set to {self.alarm_time}')
+    
+    def alarm_check(self):
+        if self.alarm_time != '0000':
+            self.alarm_on = True
+            self.alarm_off = False
+        else:
+            self.alarm_off = True
+            self.alarm_on = False
+
+    def toggle_alarm(self):
+        while self.alarm_on == True:
+            user_input = input(f'Alarm is set to {self.alarm_time}, would you like to toggle the alarm off?').lower().strip()
+            if user_input == 'no':
+                print(f'The alarm will stay on and set to {self.alarm_time}')
+                self.alarm_on = True
+                self.alarm_off = False
+                break
+            elif user_input == 'yes':
+                print('The alarm is toggled off.')
+                self.alarm_on = False
+                self.alarm_off = True
+                return False
+            else:
+                print('Unrecognized input.')
+                self.alarm_on = True
---
+++ main.py
@@ -0,0 +1,8 @@
+from ALARMCLOCK import alarm_clock
+
+test_class = alarm_clock()
+
+test_class.set_time()
+test_class.set_alarm_time()
+test_class.alarm_check()
+test_class.toggle_alarm()
